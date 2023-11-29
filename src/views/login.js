c91173123f59b38557625658b627f2af2f654424
FranklinBailey <152042890+FranklinBailey@users.noreply.github.com>
2023-11-28 18:04:42 -0700

    testing webpage

---
+++ src/views/login.js
@@ -0,0 +1,9 @@
+import Container from "react-bootstrap/esm/Container";
+import Card from "react-bootstrap/Card";
+import { Button, Nav } from "react-bootstrap";
+
+export default function Login() {
+    return(
+        <h1>Is this working?</h1>
+    )
+}
